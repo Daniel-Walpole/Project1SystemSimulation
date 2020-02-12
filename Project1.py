07619c2d5a07bdd7617da31284494eb4e3a8438e
Daniel-Walpole <57097166+Daniel-Walpole@users.noreply.github.com>
2020-02-12 17:37:11 -0500

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -43,79 +43,116 @@ class FastFood(object):
     def CookFood(self):
         return random.expovariate(1.0 / MeanCookTime)
 
-
+#function to have a customer go through line 
 def Customer(env, window, name):
+    #to use global variables use this define
     global carsServed
     global timesOfService
-    if(len(window.line1.queue) + len(window.line2.queue) < 8):
-        if (len(window.line1.queue) <= len(window.line2.queue)):
-            startTime = env.now
+    #if line 1 is shorter than line 2 go through line 1 
+    if (len(window.line1.queue) <= len(window.line2.queue)):
+        #keeping track of start time 
+        startTime = env.now
+        #ensuring the length of the queue is shorter than 4
+        if(len(window.line1.queue) < 4):
+            #getting to order window and ordering
             order = window.line1.request()
-            env.process(window.Order())
             yield order
+            env.process(window.Order())
             window.line1.release(order)
+            #getting times for food cook
             startOrder = env.now
             cookFood = window.CookFood()
+            #getting in line to pay
             req = window.payWindowLine.request()
             yield req
             window.payWindowLine.release(req)
+            #getting to window to pay
             rq = window.payWindow.request()
             yield rq
             env.process(window.Pickup())
             atWindow = env.now
+            #can only leave if food was cooked 
             if atWindow < (cookFood + startOrder):
                 yield env.timeout(abs(atWindow-cookFood+startOrder))
             window.payWindow.release(rq)
             endtime = env.now
             carsServed += 1
+            #outputing start and end times
             print('Start time for %s from line 1 {}'.format(startTime) % name)
             print('End time from line 1 {}'.format(endtime))
+            #adding completed time to list
             timesOfService.append(endtime-startTime)
-        elif(len(window.line1.queue) > len(window.line2.queue)):
-            startTime = env.now
+        else:
+            #if the queue is to long they leave
+            print('Im outta here %s' % name)
+    #if line 1 has more people in it go to line 2
+    elif(len(window.line1.queue) > len(window.line2.queue)):
+        #keeping track of start time 
+        startTime = env.now
+        #ensuring the length of the queue is shorter than 4
+        if(len(window.line2.queue) < 4):
+            #getting to order window and ordering
             order = window.line2.request()
-            env.process(window.Order())
             yield order
+            env.process(window.Order())
             window.line2.release(order)
+            #getting times for food cook
             startOrder = env.now
             cookFood = window.CookFood()
+            #getting in line to pay
             req = window.payWindowLine.request()
             yield req
             window.payWindowLine.release(req)
+            #getting to window to pay
             rq = window.payWindow.request()
             yield rq
             env.process(window.Pickup())
             atWindow = env.now
+            #can only leave if food was cooked 
             if atWindow < (cookFood + startOrder):
                 yield env.timeout(abs(atWindow-cookFood+startOrder))
-                window.payWindow.release(rq)
-            else:
-                window.payWindow.release(rq)
+            window.payWindow.release(rq)
             endtime = env.now
             carsServed += 1
+            #outputing start and end times
             print('Start time for %s from line 2 {}'.format(startTime) % name)
             print('End time from line 2 {}'.format(endtime))
+            #adding completed time to list
             timesOfService.append(endtime-startTime)
-    else:
-        print('Im outta here %s' % name)
+        else:
+            #if the queue is to long they leave
+            print('Im outta here %s' % name)
 
-
+#function to start the whole simulation
 def Start(env, AR):
+    #for use of global variables 
     global cars
     global carsServed
+    #making the resturant
     stuff = FastFood(env)
+    #run until out of time
     while True:
+        #nameing cars and adding to total cars in simulation
         name = cars + 1
         cars += 1
+        #passing time for cars randomly spawn
         yield env.timeout(random.expovariate(1.0 / AR))
+        #having them go and attempt to order food
         env.process(Customer(env, stuff, name))
+
+#setting random seed can be changed 
 random.seed(123456)
+#declaring and starting the simulation with calls
 env = simpy.Environment()
 env.process(Start(env, AR))
+#setting simulation to end after set amount of time
 env.run(until=Time)
+
+#printing useful information 
 print('%s cars seen' % cars)
 print('%s cars got their food' % carsServed)
 print(timesOfService)
+#getting mean time of cars who were served
 sum = 0.0
 for i in range(len(timesOfService)):
     sum += timesOfService[i]
